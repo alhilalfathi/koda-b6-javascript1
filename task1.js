ac7dd7e5df75fb6e2152807c3af4d2aba2a671fc
alhilalfathi <132118871+alhilalfathi@users.noreply.github.com>
2025-12-24 15:22:13 +0700

    Add files via upload

--- task1.js
+++ task1.js
@@ -1,5 +1,13 @@
-let r = 14
-const phi = 3.14
+let r = 15
+let phi
+
+if(r % 7 == 0){
+     phi = 22/7
+     console.log("Phi = 22/7")
+}else{
+     phi = 3.14
+     console.log("Phi = 3.14")
+}
 
 const luas = phi*r*r
 const keliling = 2*phi*r
